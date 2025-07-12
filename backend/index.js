d1c863e6df5188a54ff4d9deaada8c398da1d8c2
kevin Arockiam <kevinarockiam806@gmail.com>
2025-07-12 17:15:54 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 // middleware to handle cors
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://expense-tracker-kappa-sable.vercel.app",
     })
 );
 
